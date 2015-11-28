5965b7d80f3da9094a98372b528b496e5d80d4ba
DaniloVlad <danilo.vladicic@gmail.com>
2015-11-28 05:17:02 -0500

    More changes
    Seperate nav bar.

--- index.php
+++ index.php
@@ -25,35 +25,7 @@
 	</head>
 	<body>
 	
-	<!-- Start Header
-	================================================== -->
-	<header id="header" class="navbar navbar-inverse navbar-fixed-top" role="banner">
-	  <div class="container">
-	    <div class="navbar-header">
-	      <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".bs-navbar-collapse">
-	        <span class="sr-only">Toggle navigation</span>
-	        <span class="icon-bar"></span>
-	        <span class="icon-bar"></span>
-	        <span class="icon-bar"></span>
-	      </button>
-	      <!-- Your Logo -->
-	      <a href="" class="navbar-brand"><img class="logo1" src="assets/images/BitForMe-72.png"></a>
-	    </div>
-	    <!-- Start Navigation -->
-	    <nav class="collapse navbar-collapse bs-navbar-collapse navbar-right" role="navigation">
-	      <ul class="nav navbar-nav">
-	        <li>
-	          <a href="indexLog.html">Log In</a>
-	        </li>
-	        <li>
-	          <a href="#">Sign Up</a>
-	        </li>
-	      </ul>
-	    </nav>
-	  </div>
-	</header>
-	<!-- ==================================================
-	End Header -->
+	<?php include('nav.php') ?>
 	
 	<!-- Start Hero Section
 	================================================== -->
