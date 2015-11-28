75c074b3d9632344125955099946693df5d010ef
root <root@BitFor.me>
2015-11-28 07:22:59 -0500

    Update DB with auth settings

--- includes/db.php
+++ includes/db.php
@@ -2,5 +2,6 @@
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'hwestern');
 define('DB_USERNAME', 'root');
-$odb = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USERNAME);
+define('DB_PASSWORD', 'S/JlTyq0iR/7IKOYZd49knY+7hs9O23gdRvZjiHOlNY');
+$odb = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USERNAME, DB_PASSWORD);
 ?>
