9dae17d0704298bcbc592e2d0aeff8def24cdb2f
DaniloVlad <danilo.vladicic@gmail.com>
2015-11-29 05:55:26 -0500

    Took out our pass
    Security Risk lulz

--- includes/db.php
+++ includes/db.php
@@ -2,6 +2,6 @@
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'hwestern');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'S/JlTyq0iR/7IKOYZd49knY+7hs9O23gdRvZjiHOlNY');
+//define('DB_PASSWORD', 'NOT PASSWORD');
 $odb = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USERNAME, DB_PASSWORD);
 ?>
