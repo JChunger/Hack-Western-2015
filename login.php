3e7f73b0588f866951c47b7dc25ed8b1fbdd71d1
jmichaelbarker <jmichaelbarker@me.com>
2015-11-28 20:09:13 -0500

    Add <br>

--- login.php
+++ login.php
@@ -92,6 +92,7 @@ if (isset($_POST['logbtn'])) {
                         <input type="text" class="form-control" name="username" placeholder="Email Address" required="" autofocus="" />
                         <input type="password" class="form-control" name="password" placeholder="Password" required=""/>      
                         <button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
+                        <br>
                         <a class="logsign" href="register.php">Don't have an account? Sign up here!</a>
                         </form>
                     </div>
